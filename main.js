a145c68bfcfdbcb5849022d080f27456584ced9d
Ryan <116980194+RSnow20@users.noreply.github.com>
2022-11-08 21:44:42 -0500

    Added the basic grid and the drawing method

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+let sizeX = 16;
+let sizeY = 16;
+let grid = new Array(sizeX);
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+for (var i = 0; i < grid.length; i++) {
+  grid[i] = new Array(sizeY);
+}
 
-setupCounter(document.querySelector('#counter'))
+function createGrid(sizeX, sizeY) {
+  let gridWrapper = document.querySelector("#grid-wrapper");
+
+  for (let i = 0; i < sizeX; i++) {
+    for (let j = 0; j < sizeY; j++) {
+      let div = document.createElement("div");
+      div.setAttribute("id", i + "," + j);
+      div.classList.add("grid-square");
+      div.addEventListener("mouseover", function () {
+        div.classList.add("colored-square");
+      });
+      gridWrapper.appendChild(div);
+    }
+  }
+}
+
+createGrid(sizeX, sizeY);
