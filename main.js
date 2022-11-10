062e725e50df8ff71a79fd92f47e58d9cd322b41
Ryan <116980194+RSnow20@users.noreply.github.com>
2022-11-09 20:30:31 -0500

    Added settings div with grid size adjustment button

--- main.js
+++ main.js
@@ -6,6 +6,22 @@ for (var i = 0; i < grid.length; i++) {
   grid[i] = new Array(sizeY);
 }
 
+function init() {
+  createSettings();
+  createGrid(sizeX, sizeY);
+}
+
+function createSettings() {
+  let settingsWrapper = document.querySelector("#settings-wrapper");
+
+  let buttonGridSize = document.createElement("button");
+  buttonGridSize.innerText = "Change Grid Size";
+  buttonGridSize.addEventListener("click", function () {
+    changeGridSize();
+  });
+  settingsWrapper.appendChild(buttonGridSize);
+}
+
 function createGrid(sizeX, sizeY) {
   let gridWrapper = document.querySelector("#grid-wrapper");
 
@@ -22,4 +38,20 @@ function createGrid(sizeX, sizeY) {
   }
 }
 
-createGrid(sizeX, sizeY);
+function changeGridSize() {
+  let gridWrapper = document.querySelector("#grid-wrapper");
+
+  while (gridWrapper.firstChild) {
+    gridWrapper.removeChild(gridWrapper.lastChild);
+  }
+
+  let gridSize = prompt("Grid size? (Max size is 100x100)", 16);
+  if (gridSize > 100) {
+    gridSize = 100;
+  }
+
+  gridWrapper.style.gridTemplateColumns = "repeat(" + gridSize + ", 1fr)";
+  createGrid(gridSize, gridSize);
+}
+
+init();
